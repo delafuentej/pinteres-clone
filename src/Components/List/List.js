7057a8a84952aa72bc4d9220adfe5dd7f8f165d0
Jesus de la Fuente <jfuentelucas@gmail.com>
2021-11-27 13:15:30 +0100

    new data in data.json

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,13 +1,17 @@
 import React from  "react";
 import data from "../../data.json";
-import Card from "../Card/Card"
+import Card from "../Card/Card";
 const List=()=>{
     return (
         <>
         {data.map(card=>{
             return(
 
-                <Card key={card._id} img={card.img} name={card.name}/>
+                <Card 
+                key={card._id} 
+                img={card.img} 
+                name={card.name}
+                />
             )
         })}
         </>
