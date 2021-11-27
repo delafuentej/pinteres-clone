d57d04110e7da604ae226e39587391790a999ead
Jesus de la Fuente <jfuentelucas@gmail.com>
2021-11-27 10:39:24 +0100

    json file problem solved + style nav

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Card=()=>{
+
+    return <h2>Card</h2>
+}
+
+export default Card;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./Navbar.scss"
+
+const Navbar=()=>{
+    return (
+    
+            <nav>
+                <input 
+                type="text" 
+                placeholder="search"
+                className="searchInput"
+                />
+
+
+            </nav>
+
+    
+    )
+
+}
+
+export default Navbar;
